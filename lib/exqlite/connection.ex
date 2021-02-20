1d48cad04eac512a0f3625fc97bae9391099c2ca
Dmitrii Dimandt <dmitrii@dmitriid.com>
2021-02-20 19:25:26 +0100

    feat(connection): begin transaction + commit + rollback

--- lib/exqlite/connection.ex
+++ lib/exqlite/connection.ex
@@ -67,4 +67,74 @@ defmodule Exqlite.Connection do
     end
   end
 
+  @doc """
+  Begin a transaction.
+
+  For full info refer to sqlite docs: https://sqlite.org/lang_transaction.html
+
+  Note: default transaction mode is DEFERRED.
+  """
+  @impl true
+  def handle_begin(opts, state) do
+    # TODO: track nested transactions.
+    #       sqlite doesn't nest transactions with BEGIN... COMMIT...,
+    #       use the SAVEPOINT and RELEASE commands.
+
+    # TODO: handle/track SAVEPOINT
+    case Keyword.get(opts, :mode, :deferred) do
+      :immediate -> handle_transaction(:begin, "BEGIN IMMEDIATE TRANSACTION", state)
+      :exclusive -> handle_transaction(:begin, "BEGIN EXCLUSIVE TRANSACTION", state)
+      mode when mode in [nil, :deferred] -> handle_transaction(:begin, "BEGIN TRANSACTION", state)
+    end
+  end
+
+  @impl true
+  def handle_commit(_opts, state) do
+    # TODO: once we handle SAVEPOINT, we need to handle RELEASE
+    #       COMMIT TO
+    #       Meanwhile we just COMMIT
+    #       see https://sqlite.org/lang_transaction.html
+    handle_transaction(:commit, "COMMIT", state)
+  end
+
+  @impl true
+  def handle_rollback(_opts, state) do
+    # TODO: once we handle SAVEPOINT, we need to handle ROLLBACK TO
+    #       Meanwhile we just ROLLBACK
+    #       see https://sqlite.org/lang_transaction.html
+    handle_transaction(:rollback, "ROLLBACK", state)
+  end
+
+  ###----------------------------------
+  #     Internal functions and helpers
+  ###----------------------------------
+
+  defp do_connect(path) do
+    case Sqlite3.open(path) do
+      {:ok, db} ->
+        state = %__MODULE__{
+          db: db
+        }
+
+        {:ok, state}
+
+      {:error, reason} ->
+        {:error, %Exqlite.Error{message: reason}}
+    end
+  end
+
+  defp handle_transaction(call, statement, state) do
+    case Sqlite3.execute(state.db, statement) do
+      :ok ->
+        result = %Result{
+          command: call
+        }
+
+        # TODO: if we track transactions, update state here
+        {:ok, statement, result, state}
+
+      {:error, {code, reason}} ->
+        {:error, %Error{message: "#{reason}. Code: #{code}"}}
+    end
+  end
 end
