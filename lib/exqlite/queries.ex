4dc5f032e04ae47f87796c30ccde52d33a1b2f9b
Matthew Johnston <warmwaffles@gmail.com>
2021-02-21 22:47:45 -0600

    Add TODO note about query and queries

--- lib/exqlite/queries.ex
+++ lib/exqlite/queries.ex
@@ -3,6 +3,13 @@ defmodule Exqlite.Queries do
   The interface to manage cached prepared queries.
   """
 
+  #
+  # TODO: We should probably do some tracking on the number of statements being
+  #       generated and culling the oldest cached value (LRU). In its current
+  #       implementation, this could just have a run away memory leak if we are
+  #       not careful.
+  #
+
   alias Exqlite.Query
 
   @type cache :: :ets.tid()
