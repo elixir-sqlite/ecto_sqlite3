411b47424bab35f45685c5722fdcbc2918bc90b2
Matthew Johnston <warmwaffles@gmail.com>
2021-02-22 16:40:22 -0600

    Adds simple child_spec/1 definition

--- lib/ecto/adapters/exqlite/connection.ex
+++ lib/ecto/adapters/exqlite/connection.ex
@@ -2,6 +2,12 @@ defmodule Ecto.Adapters.Exqlite.Connection do
   @behaviour Ecto.Adapters.SQL.Connection
 
   @impl true
+  def child_spec(opts) do
+    {:ok, _} = Application.ensure_all_started(:db_connection)
+    DBConnection.child_spec(Exqlite.Connection, opts)
+  end
+
+  @impl true
   def query(conn, sql, params, opts) do
     opts = Keyword.put_new(opts, :query_type, :binary_then_text)
 
