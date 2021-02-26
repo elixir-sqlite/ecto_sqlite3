a4df53f67be2bc7299196ac95e51c11f28c0f7f1
Matthew Johnston <warmwaffles@gmail.com>
2021-02-25 22:30:27 -0600

    Begin work on the initial stream interface

--- lib/exqlite/stream.ex
+++ lib/exqlite/stream.ex
@@ -1,24 +1,34 @@
 defmodule Exqlite.Stream do
   @moduledoc false
-  defstruct [:conn, :query, :params, :options, max_rows: 500]
+  defstruct [:conn, :query, :params, :options]
   @type t :: %Exqlite.Stream{}
 
   defimpl Enumerable do
-    def reduce(
-          %Exqlite.Stream{query: statement, conn: conn, params: params, options: opts},
-          acc,
-          fun
-        )
-        when is_binary(statement) do
+    def reduce(%Exqlite.Stream{query: %Exqlite.Query{} = query} = stream, acc, fun) do
+      %Exqlite.Stream{conn: conn, params: params, options: opts} = stream
+
+      stream = %DBConnection.Stream{
+        conn: conn,
+        query: query,
+        params: params,
+        opts: opts
+      }
+
+      DBConnection.reduce(stream, acc, fun)
+    end
+
+    def reduce(%Exqlite.Stream{query: statement} = stream, acc, fun) do
+      %Exqlite.Stream{conn: conn, params: params, options: opts} = stream
       query = %Exqlite.Query{name: "", statement: statement}
 
-      case DBConnection.prepare_execute(conn, query, params, opts) do
-        {:ok, _, %{rows: _rows} = result} ->
-          Enumerable.reduce([result], acc, fun)
+      stream = %DBConnection.PrepareStream{
+        conn: conn,
+        query: query,
+        params: params,
+        opts: opts
+      }
 
-        {:error, err} ->
-          raise err
-      end
+      DBConnection.reduce(stream, acc, fun)
     end
 
     def member?(_, _), do: {:error, __MODULE__}
