6d6c6eeecd439de7cf929b64ca35abc9b915c541
Matthew Johnston <warmwaffles@gmail.com>
2021-02-28 08:18:12 -0600

    Remove PoolRepo for now

--- test/support/repo.ex
+++ test/support/repo.ex
@@ -1,29 +1,5 @@
-defmodule Ecto.Integration.Repo do
-  defmacro __using__(opts) do
-    quote do
-      use Ecto.Repo, unquote(opts)
-
-      @query_event __MODULE__
-                   |> Module.split()
-                   |> Enum.map(&(&1 |> Macro.underscore() |> String.to_atom()))
-                   |> Kernel.++([:query])
-
-      def init(_, opts) do
-        fun = &Ecto.Integration.Repo.handle_event/4
-        :telemetry.attach_many(__MODULE__, [[:custom], @query_event], fun, :ok)
-        {:ok, opts}
-      end
-    end
-  end
-
-  def handle_event(event, latency, metadata, _config) do
-    handler = Process.delete(:telemetry) || fn _, _, _ -> :ok end
-    handler.(event, latency, metadata)
-  end
-end
-
 defmodule Ecto.Integration.TestRepo do
-  use Ecto.Integration.Repo, otp_app: :exqlite, adapter: Ecto.Adapters.Exqlite
+  use Ecto.Repo, otp_app: :exqlite, adapter: Ecto.Adapters.Exqlite
 
   def create_prefix(prefix) do
     "create database #{prefix}"
@@ -36,8 +12,4 @@ defmodule Ecto.Integration.TestRepo do
   def uuid do
     Ecto.UUID
   end
-end
-
-defmodule Ecto.Integration.PoolRepo do
-  use Ecto.Integration.Repo, otp_app: :exqlite, adapter: Ecto.Adapters.Exqlite
 end
