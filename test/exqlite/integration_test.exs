a52a5ec85f9da14f86ae22b7365f9e8ddb74ba3d
Matthew Johnston <warmwaffles@gmail.com>
2021-03-09 09:54:58 -0600

    Attempt to recreate issue in #42

--- test/exqlite/integration_test.exs
+++ test/exqlite/integration_test.exs
@@ -3,6 +3,7 @@ defmodule Exqlite.IntegrationTest do
 
   alias Exqlite.Connection
   alias Exqlite.Sqlite3
+  alias Exqlite.Query
 
   test "simple prepare execute and close" do
     path = Temp.path!()
@@ -21,6 +22,112 @@ defmodule Exqlite.IntegrationTest do
 
     {:ok, _, conn} = Connection.handle_close(query, [], conn)
     assert conn
+
+    File.rm(path)
+  end
+
+  test "transaction handling with concurrent connections" do
+    path = Temp.path!()
+
+    {:ok, conn1} =
+      Connection.connect(
+        database: path,
+        journal_mode: :wal,
+        cache_size: -64000,
+        temp_store: :memory
+      )
+
+    {:ok, conn2} =
+      Connection.connect(
+        database: path,
+        journal_mode: :wal,
+        cache_size: -64000,
+        temp_store: :memory
+      )
+
+    {:ok, _result, conn1} = Connection.handle_begin([], conn1)
+    assert conn1.transaction_status == :transaction
+    query = %Query{statement: "create table foo(id integer, val integer)"}
+    {:ok, _query, _result, conn1} = Connection.handle_execute(query, [], [], conn1)
+    {:ok, _result, conn1} = Connection.handle_rollback([], conn1)
+    assert conn1.transaction_status == :idle
+
+    {:ok, _result, conn2} = Connection.handle_begin([], conn2)
+    assert conn2.transaction_status == :transaction
+    query = %Query{statement: "create table foo(id integer, val integer)"}
+    {:ok, _query, _result, conn2} = Connection.handle_execute(query, [], [], conn2)
+    {:ok, _result, conn2} = Connection.handle_rollback([], conn2)
+    assert conn2.transaction_status == :idle
+
+    File.rm(path)
+  end
+
+  test "transaction with interleaved connections" do
+    path = Temp.path!()
+
+    {:ok, conn1} =
+      Connection.connect(
+        database: path,
+        journal_mode: :wal,
+        cache_size: -64000,
+        temp_store: :memory
+      )
+
+    {:ok, conn2} =
+      Connection.connect(
+        database: path,
+        journal_mode: :wal,
+        cache_size: -64000,
+        temp_store: :memory
+      )
+
+    {:ok, _result, conn1} = Connection.handle_begin([mode: :immediate], conn1)
+    query = %Query{statement: "create table foo(id integer, val integer)"}
+    {:ok, _query, _result, conn1} = Connection.handle_execute(query, [], [], conn1)
+
+    # transaction overlap
+    {:ok, _result, conn2} = Connection.handle_begin([], conn2)
+    assert conn2.transaction_status == :transaction
+    {:ok, _result, conn1} = Connection.handle_rollback([], conn1)
+    assert conn1.transaction_status == :idle
+
+    query = %Query{statement: "create table foo(id integer, val integer)"}
+    {:ok, _query, _result, conn2} = Connection.handle_execute(query, [], [], conn2)
+    {:ok, _result, conn2} = Connection.handle_rollback([], conn2)
+    assert conn2.transaction_status == :idle
+
+    Connection.disconnect(nil, conn1)
+    Connection.disconnect(nil, conn2)
+
+    File.rm(path)
+  end
+
+  test "transaction handling with single connection" do
+    path = Temp.path!()
+
+    {:ok, conn1} =
+      Connection.connect(
+        database: path,
+        journal_mode: :wal,
+        cache_size: -64000,
+        temp_store: :memory
+      )
+
+    {:ok, _result, conn1} = Connection.handle_begin([], conn1)
+    assert conn1.transaction_status == :transaction
+
+    query = %Query{statement: "create table foo(id integer, val integer)"}
+    {:ok, _query, _result, conn1} = Connection.handle_execute(query, [], [], conn1)
+    {:ok, _result, conn1} = Connection.handle_rollback([], conn1)
+    assert conn1.transaction_status == :idle
+
+    {:ok, _result, conn1} = Connection.handle_begin([], conn1)
+    assert conn1.transaction_status == :transaction
+
+    query = %Query{statement: "create table foo(id integer, val integer)"}
+    {:ok, _query, _result, conn1} = Connection.handle_execute(query, [], [], conn1)
+    {:ok, _result, conn1} = Connection.handle_rollback([], conn1)
+    assert conn1.transaction_status == :idle
 
     File.rm(path)
   end
