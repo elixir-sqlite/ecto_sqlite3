43cc05ddedb1a60d87649219487752e3c569c882
Matthew Johnston <warmwaffles@gmail.com>
2021-08-25 08:07:53 -0500

    Bump to 0.6.0

--- README.md
+++ README.md
@@ -16,7 +16,7 @@ in Hexdocs.
 
 ```elixir
 defp deps do
-  {:ecto_sqlite3, "~> 0.5.6"}
+  {:ecto_sqlite3, "~> 0.6.0"}
 end
 ```
 
--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule EctoSQLite3.MixProject do
   use Mix.Project
 
-  @version "0.5.7"
+  @version "0.6.0"
 
   def project do
     [
