c72aa1030abf28ded97019e1d9287182fd04a4f5
Greg Rychlewski <greg.rychlewski@gmail.com>
2022-08-04 11:12:10 -0400

    Allow hints on joins (#83)
    Co-authored-by: Greg Rychlewski <grychle@LLL7WPM64V5M.ngco.com>

---
+++ integration_test/hints_test.exs
@@ -0,0 +1,23 @@
+defmodule Ecto.Integration.HintsTest do
+  use Ecto.Integration.Case, async: true
+
+  import Ecto.Query, only: [from: 2]
+
+  alias Ecto.Integration.Post
+  alias Ecto.Integration.TestRepo
+
+  test "join hints" do
+    {:ok, _} = TestRepo.query("CREATE INDEX post_id_idx ON posts (id)")
+    TestRepo.insert!(%Post{id: 1})
+
+    results =
+      from(p in Post,
+        join: p2 in Post,
+        on: p.id == p2.id,
+        hints: ["INDEXED BY post_id_idx"]
+      )
+      |> TestRepo.all()
+
+    assert [%Post{id: 1}] = results
+  end
+end
