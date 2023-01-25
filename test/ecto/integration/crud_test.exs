ed3d69620bdc6d18179e7add4e78eec812b0753d
Jason Stiebs <jason@fly.io>
2023-01-25 10:12:55 -0600

    fixing broken test

--- test/ecto/integration/crud_test.exs
+++ test/ecto/integration/crud_test.exs
@@ -218,7 +218,7 @@ defmodule Ecto.Integration.CrudTest do
   describe "select" do
     test "can handle in" do
       TestRepo.insert!(%Account{name: "hi"})
-      assert [] = TestRepo.all(from(a in Account, where: a.name in ["404"]))
+      assert [] = TestRepo.all(from(a in Account, where: a.name in [^"404"]))
       assert [_] = TestRepo.all(from(a in Account, where: a.name in [^"hi"]))
     end
 
