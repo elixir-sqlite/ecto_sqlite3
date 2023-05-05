3d185b973ffe3e1ddd1f2b1aa09be82a33e3325e
Joel Jucá <joelwallis@gmail.com>
2023-05-05 13:04:39 -0300

    Add tests for Ecto.Adapters.SQLite3.Codec.date_encode/2

--- test/ecto/adapters/sqlite3/codec_test.exs
+++ test/ecto/adapters/sqlite3/codec_test.exs
@@ -176,4 +176,21 @@ defmodule Ecto.Adapters.SQLite3.CodecTest do
       end
     end
   end
+
+  describe ".date_encode/2" do
+    setup do
+      [
+        date: ~D[2011-01-09],
+        datetime: ~U[2011-01-09 08:46:08.00Z]
+      ]
+    end
+
+    test "on %Date{} structs", %{date: date} do
+      {:ok, "2011-01-09"} = Codec.date_encode(date, :iso8601)
+    end
+
+    test "on %DateTime{} structs", %{datetime: datetime} do
+      {:ok, "2011-01-09"} = Codec.date_encode(datetime, :iso8601)
+    end
+  end
 end
