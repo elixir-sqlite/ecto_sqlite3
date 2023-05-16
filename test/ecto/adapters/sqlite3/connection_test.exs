09eaa033b2f475452ba52aff6ae7ddf88cba89cf
Dino Kovač <dino@latinum.xyz>
2023-05-16 07:40:47 +0200

    Make test data more realistic

--- test/ecto/adapters/sqlite3/connection_test.exs
+++ test/ecto/adapters/sqlite3/connection_test.exs
@@ -2417,22 +2417,34 @@ defmodule Ecto.Adapters.SQLite3.ConnectionTest do
   describe "to_constraints/2" do
     alias Ecto.Adapters.SQLite3.Connection
 
-    test "simple unique index" do
-      error = %Exqlite.Error{message: "UNIQUE constraint failed: one.two.three"}
-      assert Connection.to_constraints(error, []) == [unique: "one_two_three_index"]
+    test "unique index" do
+      # created with:
+      # CREATE UNIQUE INDEX users_email_name_index ON users (email);
+
+      error = %Exqlite.Error{message: "UNIQUE constraint failed: users.email"}
+      assert Connection.to_constraints(error, []) == [unique: "users_email_index"]
     end
 
     test "multi-column unique index" do
-      error = %Exqlite.Error{message: "UNIQUE constraint failed: one.two, one.three"}
-      assert Connection.to_constraints(error, []) == [unique: "one_two_three_index"]
+      # created with:
+      # CREATE UNIQUE INDEX users_email_name_index ON users (email, name);
+
+      error = %Exqlite.Error{
+        message: "UNIQUE constraint failed: users.email, users.name"
+      }
+
+      assert Connection.to_constraints(error, []) == [unique: "users_email_name_index"]
     end
 
     test "complex unique index" do
+      # created with:
+      # CREATE UNIQUE INDEX users_email_year_index ON users (email, strftime('%Y', inserted_at));
+
       error = %Exqlite.Error{
-        message: "UNIQUE constraint failed: index 'one_two_three_index'"
+        message: "UNIQUE constraint failed: index 'users_email_year_index'"
       }
 
-      assert Connection.to_constraints(error, []) == [unique: "one_two_three_index"]
+      assert Connection.to_constraints(error, []) == [unique: "users_email_year_index"]
     end
   end
 
