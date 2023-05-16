ba8c2d47287a1aadc4b40079f44fb7d5e7828f84
Dino Kovač <reisub@users.noreply.github.com>
2023-05-16 15:16:12 +0200

    Support other unique constraint error format (#115)
    Unique constraints sometimes fail with the following error format:
    
        UNIQUE constraint failed: index '<index_name>'
    
    I believe this happens when the index name doesn't match
    the names of the columns involved.
    
    Example of an index for which this happens:
    
        CREATE UNIQUE INDEX test_user_id_number_year_index
        ON invoices (user_id, number, strftime('%Y', created_at));

--- test/ecto/adapters/sqlite3/connection_test.exs
+++ test/ecto/adapters/sqlite3/connection_test.exs
@@ -2414,6 +2414,40 @@ defmodule Ecto.Adapters.SQLite3.ConnectionTest do
     assert execute_ddl(integer) == [~s/CREATE TABLE "posts" ("id" INTEGER PRIMARY KEY)/]
   end
 
+  describe "to_constraints/2" do
+    alias Ecto.Adapters.SQLite3.Connection
+
+    test "unique index" do
+      # created with:
+      # CREATE UNIQUE INDEX users_email_name_index ON users (email);
+
+      error = %Exqlite.Error{message: "UNIQUE constraint failed: users.email"}
+      assert Connection.to_constraints(error, []) == [unique: "users_email_index"]
+    end
+
+    test "multi-column unique index" do
+      # created with:
+      # CREATE UNIQUE INDEX users_email_name_index ON users (email, name);
+
+      error = %Exqlite.Error{
+        message: "UNIQUE constraint failed: users.email, users.name"
+      }
+
+      assert Connection.to_constraints(error, []) == [unique: "users_email_name_index"]
+    end
+
+    test "complex unique index" do
+      # created with:
+      # CREATE UNIQUE INDEX users_email_year_index ON users (email, strftime('%Y', inserted_at));
+
+      error = %Exqlite.Error{
+        message: "UNIQUE constraint failed: index 'users_email_year_index'"
+      }
+
+      assert Connection.to_constraints(error, []) == [unique: "users_email_year_index"]
+    end
+  end
+
   defp remove_newlines(string) do
     string |> String.trim() |> String.replace("\n", " ")
   end
