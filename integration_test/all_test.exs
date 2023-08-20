3efae274899e3a6149d7aee205116b6b00379167
Matthew Johnston <warmwaffles@gmail.com>
2023-08-19 21:41:20 -0500

    Add notes to integration test helper

--- integration_test/all_test.exs
+++ integration_test/all_test.exs
@@ -10,12 +10,20 @@ Code.require_file "#{ecto}/integration_test/cases/type.exs", __DIR__
 Code.require_file "#{ecto}/integration_test/cases/repo.exs", __DIR__
 
 ecto_sql = Mix.Project.deps_paths()[:ecto_sql]
+
+#
+# The are not supported by SQLite3 unfortunately
+#
+# Code.require_file "#{ecto_sql}/integration_test/sql/alter.exs", __DIR__
 # Code.require_file "#{ecto_sql}/integration_test/sql/lock.exs", __DIR__
 # Code.require_file "#{ecto_sql}/integration_test/sql/logging.exs", __DIR__
+# Code.require_file "#{ecto_sql}/integration_test/sql/query_many.exs", __DIR__
+#
+
+Code.require_file "#{ecto_sql}/integration_test/sql/migration.exs", __DIR__
+Code.require_file "#{ecto_sql}/integration_test/sql/migrator.exs", __DIR__
 Code.require_file "#{ecto_sql}/integration_test/sql/sandbox.exs", __DIR__
 Code.require_file "#{ecto_sql}/integration_test/sql/sql.exs", __DIR__
 Code.require_file "#{ecto_sql}/integration_test/sql/stream.exs", __DIR__
 Code.require_file "#{ecto_sql}/integration_test/sql/subquery.exs", __DIR__
 Code.require_file "#{ecto_sql}/integration_test/sql/transaction.exs", __DIR__
-Code.require_file "#{ecto_sql}/integration_test/sql/migration.exs", __DIR__
-Code.require_file "#{ecto_sql}/integration_test/sql/migrator.exs", __DIR__
