790eaeba1624f6db695dd441fa6726b81dabc2fe
Matthew Johnston <warmwaffles@gmail.com>
2023-08-29 08:21:17 -0500

    Split datetime add into two separate tests

--- test/ecto/adapters/sqlite3/connection/datetime_add_test.exs
+++ test/ecto/adapters/sqlite3/connection/datetime_add_test.exs
@@ -4,7 +4,7 @@ defmodule Ecto.Adapters.SQLite3.Connection.DatetimeAddTest do
   import Ecto.Query
   import Ecto.Adapters.SQLite3.TestHelpers
 
-  test "datetime_add" do
+  test "add a month" do
     query =
       "schema"
       |> where([s], datetime_add(s.foo, 1, "month") > s.bar)
@@ -13,7 +13,9 @@ defmodule Ecto.Adapters.SQLite3.Connection.DatetimeAddTest do
 
     assert ~s{SELECT 1 FROM "schema" AS s0 WHERE (CAST (strftime('%Y-%m-%d %H:%M:%f000Z',s0.\"foo\",1 || ' month') AS TEXT) > s0."bar")} ==
              all(query)
+  end
 
+  test "add a month with a string cast" do
     query =
       "schema"
       |> where([s], datetime_add(type(s.foo, :string), 1, "month") > s.bar)
