5ddb7a5529e5a95fa94454ba95caaacc900e0548
Greg Rychlewski <greg.rychlewski@gmail.com>
2023-08-29 09:20:39 -0400

    Fix binary uuid casting (#121)
    Co-authored-by: Greg Rychlewski <grychle@LLL7WPM64V5M.ngco.com>

--- test/ecto/integration/uuid_test.exs
+++ test/ecto/integration/uuid_test.exs
@@ -4,6 +4,8 @@ defmodule Ecto.Integration.UUIDTest do
   alias Ecto.Integration.TestRepo
   alias EctoSQLite3.Schemas.Product
 
+  import Ecto.Query, only: [from: 2]
+
   setup do
     Application.put_env(:ecto_sqlite3, :uuid_type, :string)
     on_exit(fn -> Application.put_env(:ecto_sqlite3, :uuid_type, :string) end)
@@ -33,5 +35,41 @@ defmodule Ecto.Integration.UUIDTest do
     found = TestRepo.get(Product, product.id)
     assert found
     assert found.external_id == external_id
+  end
+
+  test "handles uuid casting with binary format" do
+    Application.put_env(:ecto_sqlite3, :uuid_type, :binary)
+    Application.put_env(:ecto_sqlite3, :binary_id_type, :binary)
+
+    external_id = Ecto.UUID.generate()
+    TestRepo.insert!(%Product{external_id: external_id, bid: external_id})
+
+    product =
+      TestRepo.one(from(p in Product, where: p.external_id == type(p.bid, Ecto.UUID)))
+
+    assert %{external_id: ^external_id} = product
+
+    product =
+      TestRepo.one(
+        from(p in Product, where: p.external_id == type(^external_id, Ecto.UUID))
+      )
+
+    assert %{external_id: ^external_id} = product
+  end
+
+  test "handles binary_id casting with binary format" do
+    Application.put_env(:ecto_sqlite3, :uuid_type, :binary)
+    Application.put_env(:ecto_sqlite3, :binary_id_type, :binary)
+
+    bid = Ecto.UUID.generate()
+    TestRepo.insert!(%Product{bid: bid, external_id: bid})
+
+    product =
+      TestRepo.one(from(p in Product, where: p.bid == type(p.external_id, :binary_id)))
+
+    assert %{bid: ^bid} = product
+
+    product = TestRepo.one(from(p in Product, where: p.bid == type(^bid, :binary_id)))
+    assert %{bid: ^bid} = product
   end
 end
