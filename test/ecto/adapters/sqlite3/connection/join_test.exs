3183b70d49f671ab8a62c406c86776579c686f07
spicychickensauce <spicychickensauce@nope.com>
2025-07-19 15:56:07 +0200

    Ensure test works across multiple OTP versions

--- test/ecto/adapters/sqlite3/connection/join_test.exs
+++ test/ecto/adapters/sqlite3/connection/join_test.exs
@@ -153,8 +153,12 @@ defmodule Ecto.Adapters.SQLite3.Connection.JoinTest do
       |> select([p, q], {p.id, q.x})
       |> plan()
 
+    # We can't hardcode the columns of the CTE here,
+    # as the order changes for different Map implementations for OTP 25 and later
+    col_names = Map.keys(types) |> Enum.join(", ")
+
     assert ~s{SELECT s0."id", v1."x" FROM "schema" AS s0 } <>
-             ~s{INNER JOIN (WITH temp_78027(y, x) AS (VALUES ($1,$2),($3,$4)) SELECT * FROM temp_78027) AS v1 } <>
+             ~s{INNER JOIN (WITH temp_78027(#{col_names}) AS (VALUES ($1,$2),($3,$4)) SELECT * FROM temp_78027) AS v1 } <>
              ~s{ON (v1."x" = s0."x") AND (v1."y" = s0."y")} ==
              all(query)
   end
